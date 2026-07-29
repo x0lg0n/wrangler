1b686b5bc98e0dc27736a4147568fd183d08eafe
x0lg0n <x0lg0n@users.noreply.github.com>
2026-07-29 14:17:30 +0000

    fix:

--- src/network.ts
+++ src/network.ts
@@ -51,7 +51,7 @@ export const NETWORK_CONFIGS: Record<NetworkId, NetworkConfig> = {
     indexerWS: 'wss://indexer.preview.midnight.network/api/v4/graphql/ws',
     node:      'https://rpc.preview.midnight.network',
     proofServer: 'http://127.0.0.1:6300',
-    faucet: 'https://midnight-tmnight-preview.nethermind.dev',
+    faucet: 'https://faucet.preview.midnight.network',
     composeServices: ['proof-server'],
   },
   preprod: {
@@ -60,7 +60,7 @@ export const NETWORK_CONFIGS: Record<NetworkId, NetworkConfig> = {
     indexerWS: 'wss://indexer.preprod.midnight.network/api/v4/graphql/ws',
     node:      'https://rpc.preprod.midnight.network',
     proofServer: 'http://127.0.0.1:6300',
-    faucet: 'https://midnight-tmnight-preprod.nethermind.dev',
+    faucet: 'https://faucet.preprod.midnight.network',
     composeServices: ['proof-server'],
   },
 };
